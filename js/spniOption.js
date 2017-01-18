87d0f27d0bfa97b9139bea5fc204c9d4995c0067
Joseph Kantel <josephkantel@gmail.com>
2017-01-17 20:08:28 -0500

    New Backgrounds

--- js/spniOption.js
+++ js/spniOption.js
@@ -190,7 +190,7 @@ $("#options-modal").on("hidden.bs.modal", function () {
 });
 
 
-$backgroundSettings = [$("#settings-background-1"), $("#settings-background-2"), $("#settings-background-3"), $("#settings-background-4"), $("#settings-background-5"), $("#settings-background-6"), $("#settings-background-7"), $("#settings-background-8")];
+$backgroundSettings = [$("#settings-background-1"), $("#settings-background-2"), $("#settings-background-3"), $("#settings-background-4"), $("#settings-background-5"), $("#settings-background-6"), $("#settings-background-7"), $("#settings-background-8"), $("#settings-background-9"), $("#settings-background-10")];
 
 
 /************************************************************
@@ -216,6 +216,8 @@ function setBackground (choice) {
         case 6: $("body").css("background-image", "url(img/background6.png)"); break;
         case 7: $("body").css("background-image", "url(img/background7.png)"); break;
         case 8: $("body").css("background-image", "url(img/background8.png)"); break;
+        case 9: $("body").css("background-image", "url(img/background9.png)"); break;
+        case 10: $("body").css("background-image", "url(img/background10.png)"); break;
 		default: $("body").css("background-image", "url(img/background1.png)");
 	}
 	setActiveOption($backgroundSettings, choice);
